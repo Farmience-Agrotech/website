6ed4944ef12dab2b2ad6bdf37232a4ed786a711b
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-10-20 20:08:22 +0530

    fix export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
-  output: 'export',
   assetPrefix: isProd ? '/website/' : '',
   basePath: isProd ? '/website' : '',
+  output: 'export', // add this line!
 };
