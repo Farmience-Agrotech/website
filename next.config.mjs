8f671dfe5e48ea8eb100e18632d938b95f50e989
Tejas MK <119612514+tejasmk-tkp@users.noreply.github.com>
2024-10-30 22:45:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default {
   assetPrefix: isProd ? '/website/' : '',
-  basePath: isProd ? '/website' : '',
+  basePath: isProd ? '/website/' : '',
   output: 'export', // add this line!
   images: {
     unoptimized: true,
