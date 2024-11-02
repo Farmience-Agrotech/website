731b5439302d0b5f99a21b6191992042ac68250e
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-11-02 16:40:45 +0530

    update http to https

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
-  assetPrefix: isProd ? 'http://farmience.com/' : '',
-  //basePath: isProd ? '/website' : '',
-  output: 'export', // add this line!
+  assetPrefix: isProd ? 'https://farmience.com/' : '',
+  output: 'export',
   images: {
     unoptimized: true,
     loader: 'imgix',
