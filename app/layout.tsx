7a8c52012f34fdda24ebb2df49ea90dfd5c71615
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-11-15 11:53:07 +0530

    clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ClarityAnalytics />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <ClarityAnalytics />
       </body>
     </html>
   );
