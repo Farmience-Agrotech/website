8b98f162f65d1ea754800226355ae29b55a71e76
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-11-16 19:33:26 +0530

    google analytics

--- app/metrics/index.tsx
+++ app/metrics/index.tsx
@@ -1,8 +1,10 @@
 import MicrosoftClarity from "./MicrosoftClarity"
+import GoogleAnalytics from "./GoogleAnalytics"
 
 const Metrics = () => (
     <>
         <MicrosoftClarity />
+        <GoogleAnalytics />
     </>
 )
 
